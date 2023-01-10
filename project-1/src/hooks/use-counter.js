6571e9febbe445dc026584ff59c3aaf0426e569a
haiderali780 <muhammadhaiderali782002@gmail.com>
2023-01-09 22:51:04 -0800

    modify the logic of forwardCounter with Custom hook

--- project-1/src/hooks/use-counter.js
+++ project-1/src/hooks/use-counter.js
@@ -1,15 +1,17 @@
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
-const useCounter=()=>{
+const useCounter = () => {
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {
-      const interval = setInterval(() => {
-        setCounter((prevCounter) => prevCounter + 1);
-      }, 1000);
-  
-      return () => clearInterval(interval);
+        const interval = setInterval(() => {
+            setCounter((prevCounter) => prevCounter + 1);
+        }, 1000);
+
+        return () => clearInterval(interval);
     }, []);
+
+    return counter;
 }
 
 export default useCounter;
