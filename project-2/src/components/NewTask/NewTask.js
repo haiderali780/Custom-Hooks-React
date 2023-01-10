8a44e3f3d50673a7d8c43f5a4d768d94acd2c36a
haiderali780 <muhammadhaiderali782002@gmail.com>
2023-01-10 01:01:51 -0800

    fixing error of http req

--- project-2/src/components/NewTask/NewTask.js
+++ project-2/src/components/NewTask/NewTask.js
@@ -12,8 +12,8 @@ const NewTask = (props) => {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
-        {
+        'https://react-http-53317-default-rtdb.firebaseio.com/tasks.json'
+        , {
           method: 'POST',
           body: JSON.stringify({ text: taskText }),
           headers: {
