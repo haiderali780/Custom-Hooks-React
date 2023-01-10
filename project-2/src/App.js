bd33f3f444c453fa4e26f5f49008665816205038
haiderali780 <muhammadhaiderali782002@gmail.com>
2023-01-10 02:14:57 -0800

    adjusting the custom hook logic

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -4,40 +4,44 @@ import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 import useHttp from './hooks/use-http';
 
+
 function App() {
-
   const [tasks, setTasks] = useState([]);
-  const transformTask = objTask => {
-    const loadedTasks = [];
 
-    for (const taskKey in objTask) {
-      loadedTasks.push({ id: taskKey, text: objTask[taskKey].text });
-    }}
-    const { isLoading,error,  sendReq:fetchTasks } = useHttp({
-        url: 'https://react-http-53317-default-rtdb.firebaseio.com/tasks.json'
-      }, transformTask);
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
-    
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
 
-    useEffect(() => {
-      fetchTasks();
-    }, []);
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
 
-    const taskAddHandler = (task) => {
-      setTasks((prevTasks) => prevTasks.concat(task));
+      setTasks(loadedTasks);
     };
 
-    return (
-      <React.Fragment>
-        <NewTask onAddTask={taskAddHandler} />
-        <Tasks
-          items={tasks}
-          loading={isLoading}
-          error={error}
-          onFetch={fetchTasks}
-        />
-      </React.Fragment>
+    fetchTasks(
+      { url: 'https://react-http-53317-default-rtdb.firebaseio.com/tasks.json' },
+      transformTasks
     );
-  }
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
 
   export default App;
--- project-2/src/hooks/use-http.js
+++ project-2/src/hooks/use-http.js
@@ -1,41 +1,36 @@
-import { useState } from "react";
+import { useState, useCallback } from 'react';
 
-const useHttp = (reqConfig, applyData) => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const sendReq = async (taskText) => {
-        setIsLoading(true);
-        setError(null);
-        try {
-            const response = await fetch(
-                reqConfig.url, {
-                method: reqConfig.method ? reqConfig.method : 'GET',
-                headers: reqConfig.header ? reqConfig.headers : {},
-                body: reqConfig.body ? JSON.stringify(reqConfig.body) : null
-            }
-            );
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
 
-            if (!response.ok) {
-                throw new Error('Request failed!');
-            }
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
 
-            const data = await response.json();
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  }, []);
 
-            applyData(data)
-        } catch (err) {
-            setError(err.message || 'Something went wrong!');
-        }
-        setIsLoading(false);
-    };
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
 
-    return (
-        {
-            isLoading,
-            error,
-            sendReq
-        }
-    )
-
-}
 export default useHttp;
